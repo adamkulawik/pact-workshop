ad6318dbae832dada24848f868c2eda955024fd3
Rafał Remisiewicz <rafal.remisiewicz@gmail.com>
2020-10-18 21:48:55 +0200

    zadanie 4 - rozwiazenie

--- csc/src/test/java/com/remisiewicz/csc/messages/outgoing/RemoteStartPactTest.java
+++ csc/src/test/java/com/remisiewicz/csc/messages/outgoing/RemoteStartPactTest.java
@@ -1,11 +1,21 @@
 package com.remisiewicz.csc.messages.outgoing;
 
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import io.micronaut.runtime.server.EmbeddedServer;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import javax.inject.Inject;
 
+@MicronautTest
+@Provider("pw-gr1-csc")
+@PactFolder("pacts")
 class RemoteStartPactTest {
 
     @Inject
@@ -13,8 +23,12 @@ class RemoteStartPactTest {
 
     @BeforeEach
     void setUpTarget(PactVerificationContext context) {
+        context.setTarget(new HttpTestTarget(embeddedServer.getHost(), embeddedServer.getPort()));
     }
 
+    @TestTemplate
+    @ExtendWith(PactVerificationInvocationContextProvider.class)
     void pactVerificationTestTemplate(PactVerificationContext context) {
+        context.verifyInteraction();
     }
 }
