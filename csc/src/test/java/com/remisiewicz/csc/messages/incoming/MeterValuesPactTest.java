b3d2eb9be783a11c1d0306e12022411761d1eb0c
Rafał Remisiewicz <rafal.remisiewicz@gmail.com>
2020-10-20 20:11:15 +0200

    zadanie 10 - rozwiazanie

--- csc/src/test/java/com/remisiewicz/csc/messages/incoming/MeterValuesPactTest.java
+++ csc/src/test/java/com/remisiewicz/csc/messages/incoming/MeterValuesPactTest.java
@@ -8,6 +8,7 @@ import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import io.micronaut.context.annotation.Property;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.pactfoundation.consumer.dsl.LambdaDsl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -36,21 +37,22 @@ class MeterValuesPactTest {
                 .path(format("/cp/%s/transactions", CHARGE_POINT_NAME))
                 .headers("Content-Type", "application/json")
                 .method("PUT")
-                .body(new PactDslJsonBody()
-                        .integerType("connectorId", 1)
-                        .integerType("transactionId", 43958743)
-                        .eachLike("meterValue")
-                            .date("timestamp", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Date.from(Instant.parse("2019-07-03T10:15:30.001Z")))
-                            .eachLike("sampledValue")
-                                .stringValue("value", "103")
-                                .stringValue("context", "Sample.Periodic")
-                                .stringValue("format", "Raw")
-                                .stringValue("measurand", "Energy.Active.Import.Register")
-                                .stringValue("phase", "L1")
-                                .stringValue("location", "Outlet")
-                                .stringValue("unit", "Wh")
-                            .closeArray()
-                        .closeArray())
+                .body(LambdaDsl.newJsonBody(body -> {
+                    body.numberValue("connectorId", 1);
+                    body.numberValue("transactionId", 43958743);
+                    body.eachLike("meterValue", meterValue -> {
+                        meterValue.date("timestamp", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Date.from(Instant.parse("2019-07-03T10:15:30.001Z")));
+                        meterValue.eachLike("sampledValue", sampledValue -> {
+                            sampledValue.stringValue("value", "103");
+                            sampledValue.stringValue("context", "Sample.Periodic");
+                            sampledValue.stringValue("format", "Raw");
+                            sampledValue.stringValue("measurand", "Energy.Active.Import.Register");
+                            sampledValue.stringValue("phase", "L1");
+                            sampledValue.stringValue("location", "Outlet");
+                            sampledValue.stringValue("unit", "Wh");
+                        });
+                    });
+                }).build())
                 .willRespondWith()
                 .status(200)
                 .body(new PactDslJsonBody().asBody())
